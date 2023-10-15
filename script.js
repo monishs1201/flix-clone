3fb40bb828ad6bbcfffdedb07043344213537e28
monishs1201 <146984440+monishs1201@users.noreply.github.com>
2023-10-15 13:39:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let add1=document.querySelector(".add1")
+let cont1=document.querySelector(".cont-1")
+add1.addEventListener("click",()=>{
+    console.log("clicked")
+    cont1.classList.toggle("active")
+})
+
+let add2=document.querySelector(".add2")
+let cont2=document.querySelector(".cont-2")
+add2.addEventListener("click",()=>{
+    console.log("clicked")
+    cont2.classList.toggle("active")
+})
+
+let add3=document.querySelector(".add3")
+let cont3=document.querySelector(".cont-3")
+add3.addEventListener("click",()=>{
+    console.log("clicked")
+    cont3.classList.toggle("active")
+})
+
+let add4=document.querySelector(".add4")
+let cont4=document.querySelector(".cont-4")
+add4.addEventListener("click",()=>{
+    console.log("clicked")
+    cont4.classList.toggle("active")
+})
+
+let add5=document.querySelector(".add5")
+let cont5=document.querySelector(".cont-5")
+add5.addEventListener("click",()=>{
+    console.log("clicked")
+    cont5.classList.toggle("active")
+})
+
+let add6=document.querySelector(".add6")
+let cont6=document.querySelector(".cont-6")
+add6.addEventListener("click",()=>{
+    console.log("clicked")
+    cont6.classList.toggle("active")
+})
